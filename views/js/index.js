ed5e1387be4cc9f77cd6d782372fc1b469b6b245
Tyler Stefanich <tyler.stefanich@gmail.com>
2017-03-08 00:27:16 -0800

    Building out front end screens
    Building out front end screens. Intro, login, sign up

--- views/js/index.js
+++ views/js/index.js
@@ -1,3 +1,174 @@
++function ($) {
+  'use strict';
+
+  if ( !$.fn.carousel ) {
+    throw new Error( "carousel-swipe required bootstrap carousel" )
+  }
+
+  // CAROUSEL CLASS DEFINITION
+  // =========================
+
+  var CarouselSwipe = function(element) {
+    this.$element    = $(element)
+    this.carousel    = this.$element.data('bs.carousel')
+    this.options     = $.extend({}, CarouselSwipe.DEFAULTS, this.carousel.options)
+    this.startX      =
+    this.startY      =
+    this.startTime   =
+    this.cycling     =
+    this.$active     =
+    this.$items      =
+    this.$next       =
+    this.$prev       =
+    this.dx          = null
+    this.sliding     = false
+
+    this.$element
+      .on('touchstart',        $.proxy(this.touchstart,this))
+      .on('touchmove',         $.proxy(this.touchmove,this))
+      .on('touchend',          $.proxy(this.touchend,this))
+      .on('slide.bs.carousel', $.proxy(this.sliding, this))
+      .on('slid.bs.carousel',  $.proxy(this.stopSliding, this))
+  }
+
+  CarouselSwipe.DEFAULTS = {
+    swipe: 50 // percent per second
+  }
+
+  CarouselSwipe.prototype.sliding = function() {
+    this.sliding = true
+  }
+
+  CarouselSwipe.prototype.stopSliding = function() {
+    this.sliding = false
+  }
+
+  CarouselSwipe.prototype.touchstart = function(e) {
+    if (this.sliding || !this.options.swipe) return;
+    var touch = e.originalEvent.touches ? e.originalEvent.touches[0] : e
+    this.dx = 0
+    this.startX = touch.pageX
+    this.startY = touch.pageY
+    this.cycling = null
+    this.width = this.$element.width()
+    this.startTime = e.timeStamp
+  }
+
+  CarouselSwipe.prototype.touchmove = function(e) {
+    if (this.sliding || !this.options.swipe) return;
+    var touch = e.originalEvent.touches ? e.originalEvent.touches[0] : e
+    var dx = touch.pageX - this.startX
+    var dy = touch.pageY - this.startY
+    if (Math.abs(dx) < Math.abs(dy)) return; // vertical scroll
+
+    if ( this.cycling === null ) {
+      this.cycling = !!this.carousel.interval
+      this.cycling && this.carousel.pause()
+    }
+
+    e.preventDefault()
+    this.dx = dx / (this.width || 1) * 100
+    this.swipe(this.dx)
+  }
+
+  CarouselSwipe.prototype.touchend = function(e) {
+    if (this.sliding || !this.options.swipe) return;
+    if (!this.$active) return; // nothing moved
+    var all = $()
+      .add(this.$active).add(this.$prev).add(this.$next)
+      .carousel_transition(true)
+
+    var dt = (e.timeStamp - this.startTime) / 1000
+    var speed = Math.abs(this.dx / dt) // percent-per-second
+    if (this.dx > 40 || (this.dx > 0 && speed > this.options.swipe)) {
+      this.carousel.prev()
+    } else if (this.dx < -40 || (this.dx < 0 && speed > this.options.swipe)) {
+      this.carousel.next();
+    } else {
+      this.$active
+        .one($.support.transition.end, function () {
+          all.removeClass('prev next')
+        })
+      .emulateTransitionEnd(this.$active.css('transition-duration').slice(0, -1) * 1000)
+    }
+
+    all.css('transform', '')
+    this.cycling && this.carousel.cycle()
+    this.$active = null // reset the active element
+  }
+
+  CarouselSwipe.prototype.swipe = function(percent) {
+    var $active = this.$active || this.getActive()
+    if (percent < 0) {
+        this.$prev
+            .css('transform', 'translate3d(0,0,0)')
+            .removeClass('prev')
+            .carousel_transition(true)
+        if (!this.$next.length || this.$next.hasClass('active')) return
+        this.$next
+            .carousel_transition(false)
+            .addClass('next')
+            .css('transform', 'translate3d(' + (percent + 100) + '%,0,0)')
+    } else {
+        this.$next
+            .css('transform', '')
+            .removeClass('next')
+            .carousel_transition(true)
+        if (!this.$prev.length || this.$prev.hasClass('active')) return
+        this.$prev
+            .carousel_transition(false)
+            .addClass('prev')
+            .css('transform', 'translate3d(' + (percent - 100) + '%,0,0)')
+    }
+
+    $active
+        .carousel_transition(false)
+        .css('transform', 'translate3d(' + percent + '%, 0, 0)')
+  }
+
+  CarouselSwipe.prototype.getActive = function() {
+    this.$active = this.$element.find('.item.active')
+    this.$items = this.$active.parent().children()
+
+    this.$next = this.$active.next()
+    if (!this.$next.length && this.options.wrap) {
+      this.$next = this.$items.first();
+    }
+
+    this.$prev = this.$active.prev()
+    if (!this.$prev.length && this.options.wrap) {
+      this.$prev = this.$items.last();
+    }
+
+    return this.$active;
+  }
+
+  // CAROUSEL PLUGIN DEFINITION
+  // ==========================
+
+  var old = $.fn.carousel
+  $.fn.carousel = function() {
+    old.apply(this, arguments);
+    return this.each(function () {
+      var $this   = $(this)
+      var data    = $this.data('bs.carousel.swipe')
+      if (!data) $this.data('bs.carousel.swipe', new CarouselSwipe(this))
+    })
+  }
+
+  $.extend($.fn.carousel,old);
+
+  $.fn.carousel_transition = function(enable) {
+    enable = enable ? '' : 'none';
+    return this.each(function() {
+      $(this)
+        .css('-webkit-transition', enable)
+        .css('transition', enable)
+    })
+  };
+
+}(jQuery);
+
 /******************************************
 
 Globals
@@ -130,6 +301,118 @@ if (navigator.geolocation) {
       handleLocationError(false, locationWindow, currentPosition);
     }
 
+
+$(window).load(function(){
+      // Message for Window Loaded
+      console.log('window loaded');
+
+      // Check cookie is set for first visit and change
+      // animation based on that.
+      //checkAndSetCookieForMenu();
+
+      //Check cookie is set for visited artworks
+      //checkAndSetCookieForVisitedArtwork();
+
+      // Fade out Loading Screen with a delay of 4 seconds
+      $('.loading-container').delay(4000).velocity({opacity: 0},1000,function(){
+        $('.page.loading').delay(500).velocity({opacity: 0},1000,function(){
+          $(this).remove();
+        });
+      });
+
+});
+
+var signUpCarouselDirection = 'null';
+$("#carousel-intro").carousel();
+$("#carousel-sign-up").carousel();
+$(".carousel-sign-up-btn").on('click tap', function () {
+  // do something…
+  var index = $('#carousel-sign-up .active').index('#carousel-sign-up .item');
+  index = index + 1;
+  var totalLength = $('#carousel-sign-up .item').length;
+
+  if(index == totalLength - 1 && signUpCarouselDirection == 'right'){
+       // Reached the end
+       $('.sign-up-continue-btn').text('Save');
+       $('.sign-up-continue-btn').addClass('btn-primary');
+  } else if(index == totalLength && signUpCarouselDirection == 'left') {
+      // Going back from the last slide
+      $('.sign-up-continue-btn').text('Continue');
+      $('.sign-up-continue-btn').removeClass('btn-primary');
+  } else if(index == 1 && signUpCarouselDirection == 'left') {
+     // Close Sign up
+     slideDownPanel($('.page.sign-up .close'));
+  }
+
+});
+
+
+$(document).ready(function(){
+     //Fix height problem with intro
+     $('#carousel-example-generic').height($(document).height()-$('.page.intro .intro-footer').height());
+
+
+     // Regenerate Avatar Button
+     $('body').on('click tap', '.regenerate-avatar-btn', function(e){
+       var totalNumberOfAvatars =  75;
+       var r = Math.round(Math.random()*totalNumberOfAvatars)
+       $('.regenerate-avatar-image').attr('src', 'assets/images/avatars/avatar_'+ r +'.png')
+     });
+
+     // Links to slide additional info
+     $('body').on('click tap', '.more-details', function(e){
+        console.log('click')
+        moreDetails($(this));
+     });
+
+     // Close Panels 
+     $('body').on('click tap', '.close', function(e){
+        slideDownPanel($(this));
+     });
+
+});
+
+
+
+
+
+function moreDetails(click){
+     var $this = null;
+     // check if object is jquery object
+     if(click instanceof jQuery){
+          $this = click;
+     } else {
+          // this is a javascript object
+          $this = $('#'+click.id);
+     }
+     var href = $this.attr("href");
+     var hash = href.substr(1);
+
+     //Set Panel from display none to display block
+     $('.page.'+hash).css('display','block');
+
+     //If we don't set a slight timeout the css animation won't work.
+     setTimeout(function(){
+          $('.page.'+hash).addClass('slideUp');
+     },100);
+}
+
+
+function slideDownPanel(link){
+     var $this = link;
+
+     if($this.parent('.nav').parent('.page').hasClass('sign-up')){
+          $('#carousel-sign-up').carousel(0);
+     }
+     $this.parent('.nav').parent('.page').removeClass('slideUp');
+     setTimeout(function(){
+       $this.parent('.nav').parent('.page').css('display','none');
+     }, 750);
+}
+
+
+
+
 /******************************************
 socket.io communication
 *******************************************/
@@ -161,5 +444,3 @@ socket.on('disconnect', function(){
 console.log('disconnected from the server as: ' + socket.id);
 
 });
-
-

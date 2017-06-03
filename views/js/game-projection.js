4821ef471927db3398225538ecf16296ae3047b7
Tyler Stefanich <tyler.stefanich@gmail.com>
2017-06-03 10:34:03 -0700

    Missing function

--- views/js/game-projection.js
+++ views/js/game-projection.js
@@ -781,6 +781,16 @@ function draw() {
 
 
 }
+function collidePointRect (pointX, pointY, x, y, xW, yW) {
+//2d
+if (pointX >= x &&         // right of the left edge AND
+    pointX <= x + xW &&    // left of the right edge AND
+    pointY >= y &&         // below the top AND
+    pointY <= y + yW) {    // above the bottom
+        return true;
+}
+return false;
+};
 
 function perlin() {
   let i = 0;
